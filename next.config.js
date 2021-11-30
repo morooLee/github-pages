93fe497afe96887edcb7fab8f44803b4ba5cd0f0
moroo <moroo@morooui-MacBookPro.local>
2021-12-01 06:36:21 +0900

    prefix url 제거

--- next.config.js
+++ next.config.js
@@ -9,8 +9,6 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 });
 
 // module.exports = {
