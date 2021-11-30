6ff33f56f55c9f6742cb0d2e831c7a77df920455
moroo <moroo@morooui-MacBookPro.local>
2021-12-01 05:58:56 +0900

    이미지 로드 경로 설정 변경

--- next.config.js
+++ next.config.js
@@ -11,9 +11,6 @@ module.exports = withMDX({
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  images: {
-    path: process.env.NEXT_PUBLIC_BASE_PATH,
-  },
 });
 
 // module.exports = {
