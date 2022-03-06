acb05be0ecaeb5bc5545d40f2aaddfebe5141321
morooLee <moroo.lee@gmail.com>
2022-03-07 03:54:47 +0900

    sitemap 추가

---
+++ next-sitemap.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://blog.moroo.dev',
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://blog.moroo.dev/server-sitemap.xml', // <==== Add here
+    ],
+  },
+};
