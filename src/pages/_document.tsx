f32793322e5f8fbd5dad1d85f9ad446bf4cd5722
morooLee <moroo.lee@gmail.com>
2022-03-29 04:42:09 +0900

    이모션 에러 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,28 +23,12 @@ class MyDocument extends Document {
     return (
       <Html lang="ko-KR">
         <Head>
-          {/* Global site tag (gtag.js) - Google Analytics */}
-          {/* <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=G-92GW2B57SC`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-92GW2B57SC');
-            `}
-          </Script> */}
-          {/* Global site tag (gtag.js) - Google Analytics */}
-
           <link rel="icon" href="/assets/favicon.png" />
           <meta name="theme-color" content="#21262d" />
-          {/* <style
+          <style
             data-emotion-css={(this.props as any).ids.join(' ')}
             dangerouslySetInnerHTML={{ __html: (this.props as any).css }}
-          /> */}
+          />
           <style
             type="text/css"
             dangerouslySetInnerHTML={{ __html: mediaStyles }}
